ba32bf68a319592154a2fc3c22acf0d65698709d
yann <yann.public@gmail.com>
2019-10-11 15:32:19 +0200

    added "--devel" build option to the homebrew formula
    + possibility to build from #develop branch
    + fixed a cmake bug (building from source directory)
    + the version tag in the "about" menu shows specific info if building from HEAD in the master or develop branches

--- scantailor.rb
+++ scantailor.rb
@@ -5,8 +5,12 @@ class Scantailor < Formula
   url "https://github.com/4lex4/scantailor-advanced/archive/v1.0.16.tar.gz"
   sha256 "84629d2edba4c36c62bdb75eedb145262b894d950bcb95cec0dab43e21bdb909"
 
-  head "https://github.com/4lex4/scantailor-advanced.git"
+  head "https://github.com/4lex4/scantailor-advanced.git", :branch => "master"
   
+	devel do
+		url "https://github.com/4lex4/scantailor-advanced.git", :branch => "develop"
+		version "DEVEL"
+	end
   depends_on "boost"
   depends_on "qt5"
   depends_on "libtiff" 
@@ -16,7 +20,23 @@ class Scantailor < Formula
   depends_on "cmake" => :build
   
   def install
-    system "cmake", ".", *std_cmake_args
-    system "make", "install"
+    vtag="#release"
+    if build.devel?
+      vtag = "#develop@HEAD (#{Time.now.utc.strftime("%Y%m%d")})"
+    elsif build.head?
+      vtag = "#master@#{version} (#{Time.now.utc.strftime("%Y%m%d")})"
+    end
+    if File.file?("version.h.in")
+      ohai "Setting versioning tag to #{vtag}"
+      inreplace "version.h.in", /#define VERSION "([^"]*)"/, "#define VERSION \"\\1#{vtag}\""
+    elsif File.file?("version.h") 
+      ohai "Setting versioning tag to #{vtag}"
+      inreplace "version.h", /#define VERSION "([^"]*)"/, "#define VERSION \"\\1#{vtag}\""
+    end
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make"
+      system "make", "install"
+    end
   end
 end
